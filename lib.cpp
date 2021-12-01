5368c334154712990b496ef3fb7f37d2de05ac38
Rcerruti <91542393+Rcerruti@users.noreply.github.com>
2021-12-01 20:59:28 +0100

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,1 +1,15 @@
+#include <iostream>
+using namespace std;
 
+bool num_primi(int a, int b){
+    if(b==1 or b==0){
+        return true;
+    }else {
+        int resto = a % b;
+        if (resto != 0) {
+            num_primi(a, b - 1);
+        }else{
+            return false;
+        }
+    }
+}
