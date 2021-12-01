54c115b72d5fab77875f8c3235c00b39ca5e6860
Rcerruti <91542393+Rcerruti@users.noreply.github.com>
2021-12-01 21:00:03 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,15 @@
 #include <iostream>
+#include "lib.h"
+using namespace std;
 
 int main(){
-  return 0;
+    int numero,divisore=0;
+    cin>>numero;
+    divisore= numero-1;
+    if(num_primi(numero, divisore)){
+        cout<<"numero primo";
+    }else{
+        cout<<"numero non primo";
+    }
+    return 0;
 }
